c7b2c6b3e18e018d46a39c55ceaf411622c4065c
lindacom <43495865+lindacom@users.noreply.github.com>
2020-02-29 14:24:31 +0000

    Create landing-nav.php

---
+++ projects/Shopping cart/landing-nav.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  
+<title>books</title>
+  
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="/css/app.scss">
+   
+	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
+      
+          <script src="js/JsLocalSearch.js"></script>  <!--used for the search box --> 
+          
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/plugins/foundation.orbit.min.js"></script> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.1.1/motion-ui.min.css" />
+
+
+  </head>
+  
+  <body>
+
+   
+ 
+  
+
+    <!--navigation --> 
+    
+    <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">The Book Store</li>
+      <li><a href="#">Return to main site</a></li>
+     
+      
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+     <li><input type="text" id="search" class="form-control input-lg" placeholder="Quick search..." />
+
+    
+     
+
+     
+      <li><a href="#" class="button">Search all books</a></li>
+    <li><a href="#">Sign in</a></li>
+    
+  
+  
+      
+    </ul>
+  </div>
+</div> 
