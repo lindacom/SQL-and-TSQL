f791bafd550a836d36bf5a2ac3d572c113df295a
lindacom <43495865+lindacom@users.noreply.github.com>
2021-05-20 18:39:15 +0100

    Create dbConnect.php

---
+++ projects/OOP-shopping-cart-TSQL/database connection/dbConnect.php
@@ -0,0 +1,5 @@
+<?php 
+
+$database = $database = new Mysql();
+$database->connect($username ="laravel", $password = "laravellaravel1", $host = "localhost", $dbname = "PHPDatabase", $options = []);
+?>
