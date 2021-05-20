c074b981275d5c43e1a89aef2819f22560a7946a
lindacom <43495865+lindacom@users.noreply.github.com>
2021-05-20 18:41:52 +0100

    Create Mysql.php

---
+++ projects/OOP-shopping-cart-TSQL/database connection/Mysql.php
@@ -0,0 +1,32 @@
+<?php
+
+include ('./interfaces/DB.php');
+?>
+
+<?php
+
+// implements the DB interface (contract).  This class must initialise connect and query methods of the interface.
+
+class Mysql implements DB
+{
+    protected $db = null;
+
+    public function connect($username ="", $password = "", $host = "localhost", $dbname = "PHPDatabase", $options = [])
+    {
+       // connection to sqlserver
+   $db = new PDO("sqlsrv:Server={$host};Database={$dbname}", $username, $password);
+    }
+
+    public function query($query)
+    {
+        return $this->db->query($query);
+    } 
+}
+
+
+
+    
+?>
+
+
+
